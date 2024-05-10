7ca974f14d76d283b8212ce9289541af02d6f316
AlexJYad <alexanderjacovlev@gmail.com>
2024-05-11 01:17:44 +0200

    Finish project

---
+++ script.js
@@ -0,0 +1,10 @@
+function testFunction() {
+    window.alert('js is working');
+}
+
+function toggleNav() {
+    const outerGrid = document.querySelector('.outer-grid');
+    outerGrid.classList.toggle('outer-grid-expanded');
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('hide-nav');
+}
